e3534e8ead355883aaf435f6b5a28cd359b00f54
Athmani <fm_athmani@esi.dz>
2019-11-21 22:20:22 +0100

    Finished Create Dataset alpha beta main

--- data.py
+++ data.py
@@ -111,14 +111,12 @@ def main():
                 path_infos = path_k.split("/")
                 instance_name = path_infos[8].replace("best_","")
                 instance_path = "../Benchmark_rm_sijk_cmax_metaheuristic/Set_ML_alpha_beta/"+path_infos[4]+"/"+path_infos[5]+"/"+path_infos[6]+"/"+instance_name+".txt"
-                print(instance_path)
-                f = open(instance_path,"r")
-                """output_file.write(write_instance(instance_path))
+                output_file.write(write_instance(instance_path))
                 output_file.write(" output ")
                 output_file.write(write_solution(path_k))
-                output_file.write("\n")"""
-    #print(write_instance("../Instances/I_100_5_S_1-149_0.1_0.4_10.txt"))
-    #print(write_solution_ML("../Instances/Solution/best_I_100_5_S_1-149_0.1_0.4_10"))
+                output_file.write("\n")
+    output_file.close()
+    
 if __name__ == '__main__':
     main()
 
